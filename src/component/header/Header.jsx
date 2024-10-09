6ec2a1fcfd8db4121c61170ecf76ebc3103b805a
Demonenog <demonito-56@mail.ru>
2024-10-10 00:24:09 +0300

    delete div className='container' <Header />

--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -4,18 +4,16 @@ import resume from './../../resume/Resume.pdf';
 const Header = () => {
 	return (
 		<header className='header'>
-			<div className='container'>
-				<div className='header__wrapper'>
-					<h1 className='header__title'>
-						<strong>
-							Hi, my name is <em>Dmitriy Pershin</em>
-						</strong>
-						<br />I am frontend <b>REACT</b> developer
-					</h1>
-					<a href={resume} download='resume' className='btn'>
-						Download Resume
-					</a>
-				</div>
+			<div className='header__wrapper'>
+				<h1 className='header__title'>
+					<strong>
+						Hi, my name is <em>Dmitriy Pershin</em>
+					</strong>
+					<br />I am frontend <b>REACT</b> developer
+				</h1>
+				<a href={resume} download='resume' className='btn'>
+					Download Resume
+				</a>
 			</div>
 		</header>
 	);
